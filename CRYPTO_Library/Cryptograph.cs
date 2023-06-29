3e2928a60eddd62869bc6f39d180b4647aab0419
Alexandr_AFONIN <sasha1999sasha93@gmail.com>
2023-06-29 20:46:30 +0300

    Upload xml manager for de/serializations

---
+++ CRYPTO_Library/Cryptograph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CRYPTO_Library
+{
+    internal static class Cryptograph
+    {
+        private static string _key;
+        private static byte[] _keyArray;
+
+        public static string Key
+        {
+            set
+            {
+                
+                _key = value;
+                _keyArray = UTF8Encoding.UTF8.GetBytes(_key);
+            }
+        }
+        public static string Encrypt(string str)
+        {
+            ICryptoTransform transform = GetAES().CreateEncryptor();
+            byte[] strBytes = UTF8Encoding.UTF8.GetBytes(str);
+            byte[] result = transform.TransformFinalBlock(strBytes, 0, strBytes.Length);
+            return Convert.ToBase64String(result,0,result.Length);
+        }
+        public static string Decrypt(string str)
+        {
+            ICryptoTransform transform = GetAES().CreateDecryptor();
+            byte[] strBytes = Convert.FromBase64String(str);
+            byte[] result = transform.TransformFinalBlock(strBytes, 0, strBytes.Length);
+            return Encoding.UTF8.GetString(result);
+        }
+
+        private static Aes GetAES()
+        {
+            Aes aes = Aes.Create();
+
+            aes.Key = _keyArray;
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
+
+            return aes;
+        }
+    }
+}
---
+++ CRYPTO_Library/XML_Manager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace CRYPTO_Library
+{
+    /// <summary>
+    /// To use XML_Manager you need create KeyGen (XML_Manager.CreateKeyGen)
+    /// </summary>
+    public class XML_Manager
+    {
+        private  string _path;
+        private XML_Manager(string path) => _path = path;
+         
+        private string SerializeObjectToSTR<T>(T objectForDesirialization)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream();
+            XmlTextWriter xmlText = new XmlTextWriter(memoryStream, Encoding.UTF8);
+            xmlSerializer.Serialize(xmlText, objectForDesirialization);
+            memoryStream = (MemoryStream) xmlText.BaseStream;
+            return GetStr(memoryStream.ToArray());
+        }
+        private object DesirializeObject<T>(string str)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream(GetBytes(str));
+            return xmlSerializer.Deserialize(memoryStream);
+        }
+        private string LoadStr()
+        {
+            using (StreamReader reader = File.OpenText(_path))
+            {
+                string str = reader.ReadToEnd();
+                return Cryptograph.Decrypt(str);
+            }
+        }
+        private void SaveStr(string str)
+        {
+            using(StreamWriter writer = File.CreateText(_path))
+            {
+                writer.Write(Cryptograph.Encrypt(str));
+            }
+        }
+        private bool HasFile => File.Exists(_path);
+        private byte[] GetBytes(string str) => UTF8Encoding.UTF8.GetBytes(str);
+
+        private string GetStr(byte[] bytes) => UTF8Encoding.UTF8.GetString(bytes);
+
+        /// <summary>
+        /// Key is 32 bit string
+        /// </summary>
+        /// <param name="key"></param>
+        public static XML_Manager CreateKeyGen(string key, string savePath)
+        {
+            Cryptograph.Key = key;
+            return new XML_Manager(key);
+        }
+        public void Save<T>(T objectForSerialization)
+        {
+            if(_path == null) throw new Exception("You should create KeyGen | Path not found..");
+            string str = SerializeObjectToSTR<T>(objectForSerialization);
+            SaveStr(str);
+        }
+        public T Load<T>(T objectForDesirialization)
+        {
+            if (HasFile){
+
+                string str = LoadStr();
+                return (T) DesirializeObject<T>(str);
+            }
+            else throw new Exception("File doesn't exist | I can load..");
+        }
+    }
+}
